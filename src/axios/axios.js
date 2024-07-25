56975d7177e1ebaaa553d20b31fa3c269322e212
rashidbekraximov <rashidbekraximov21@gmail.com>
2024-07-25 10:04:16 +0500

    initial commit

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -6,7 +6,7 @@ const http = axios.create({
     headers: {
         'Content-type': 'application/json',
         "Access-Control-Allow-Origin": "*",
-        "Authorization": `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIwMDExMTIyMzMiLCJhY3RpdmVVc2VySWQiOjEwMDAsInJvbGVzIjpbIlNZU1RFTV9ST0xFX1NVUEVSX0FETUlOIl0sImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6MTUxNS9hcGkvYXV0aC9sb2dpbiIsImV4cCI6MTcyMTU2OTMzMX0.WXEVigVg_gGhExyz-lg9zkTHjGeTgCYZeLEZLZYLCtA`,
+        // "Authorization": `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIwMDExMTIyMzMiLCJhY3RpdmVVc2VySWQiOjEwMDAsInJvbGVzIjpbIlNZU1RFTV9ST0xFX1NVUEVSX0FETUlOIl0sImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6MTUxNS9hcGkvYXV0aC9sb2dpbiIsImV4cCI6MTcyMTU2OTMzMX0.WXEVigVg_gGhExyz-lg9zkTHjGeTgCYZeLEZLZYLCtA`,
         accept: "*/*",
         "Access-Control-Allow-Methods": "*",
         "Access-Control-Allow-Headers": "*",
--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
-export const API_URL ="http://localhost:1515/api/";
-// export const API_URL ="http://185.217.131.171:3030/api/";
+// export const API_URL ="http://localhost:1515/api/";
+export const API_URL ="http://45.138.158.84:3030/api/";
